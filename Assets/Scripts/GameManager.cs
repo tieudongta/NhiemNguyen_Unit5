e61cd287b740e4b655cbb9b88345f54ef3b103d3
tieudongta <nhiem.nguyentri@gmail.com>
2021-05-07 11:10:09 -0700

    Lesson A

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private const float spawnRate = 2.0f;
+    public List<GameObject> prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(prefabs[Random.Range(0, prefabs.Count)]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private const float minForce = 10;
+    private const float maxForce = 15;
+    private const float minTorque = -10;
+    private const float maxTorque = 10;
+    private const float minXPos = -3;
+    private const float maxXPos = 3;
+    private const float ySpawnPos = -2;
+    private Rigidbody targetRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        RandomForce();
+        RandomTorque();
+        RandSpawnnPos();
+    }
+    void RandomForce()
+    {
+        targetRB.AddForce(Vector3.up * Random.Range(minForce, maxForce), ForceMode.Impulse);
+    }
+    void RandomTorque()
+    {
+        targetRB.AddTorque(Random.Range(minTorque, maxTorque), Random.Range(minTorque, maxTorque), Random.Range(minTorque, maxTorque), ForceMode.Impulse);
+    }
+    void RandSpawnnPos()
+    {
+        transform.position = new Vector3(Random.Range(minXPos, minXPos), ySpawnPos);
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Sensor"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
